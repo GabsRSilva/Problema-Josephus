849f44c1f07d70f9c1e8e85870ded372868d83e9
GabsRSilva <42253364+GabsRSilva@users.noreply.github.com>
2019-06-04 09:42:04 -0300

    Add files via upload

---
+++ GabrielleSilva_Josephus.c
@@ -0,0 +1,31 @@
+/* Programa Problema Josephus
+* Arquivo: GabrielleSilva_Josephus.c
+* Autores: Breno Góes do Amaral, Gabrielle Ramos da Silva, 
+		   Jolie Caldarone, Victor Barbosa Bulhões
+* Data:    03/06/19  17:00
+* Descrição: Arquivo source com função principal e função Lista construtora para
+             construção de uma lista com n números acessando somente a interface.
+*/
+
+#include <stdio.h>
+#include "LCDL_4.h"
+
+//interface
+//void permutacaoJosephus(Lista, int, int);  //mostra na tela a permutação de Josephus
+Lista construtoraN(int);				   //constroi uma lista com n números
+
+//principal
+void main(){
+	
+}
+
+//implementação
+Lista construtoraN(int n){
+	
+}
+
+/*
+void permutacaoJosephus(Lista L, int n, int m){  //n é o numero de pessoas no circulo e m é o passo da morte
+	
+	
+}*/
---
+++ LCDL_4.h
@@ -0,0 +1,72 @@
+/* Header Programa Problema Josephus
+* Arquivo: LCDL_4.h
+* Autores: Breno Góes do Amaral, Gabrielle Ramos da Silva, 
+		   Jolie Caldarone, Victor Barbosa Bulhões
+* Data:    03/06/19  17:00
+* Descrição: Arquivo header com as funções de criação da lista circular duplamente ligada
+			 e da permtação do Josephus
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "Booleano"
+
+
+typedef struct Celula{
+	int item;
+	struct Celula* seguinte;
+	struct Celula* anterior;
+}Celula;
+
+typedef Celula * Lista;
+
+//interface
+Lista criarListaVazia(); 			//cria uma lista vazia
+bool verificarVazia(Lista); 		//devolve TRUE se a lista for vazia
+void mostrarLista(Lista);			//mostra os elementos da lista em sentido horário
+Lista inserir(Lista, int;			//acrescenta item no inicio da lista
+Lista inserirFimLista(Lista, int); //acrescenta item no fim da lista
+Lista remover(Lista, Celula *);		//remove celula referenciada
+Lista esvaziar(Lista);				//remove todos os itens da lista
+
+void permutacaoJosephus(Lista, int, int);  //mostra na tela a permutação de Josephus
+
+//implementação
+Lista criarListaVazia(){
+	
+	return L;
+}
+
+bool verificarVazia(Lista L){
+	bool vazia;
+	
+	return vazia;
+}
+
+void mostrarLista(Lista L){
+	
+}
+
+Lista inserir(Lista L, int n){
+	
+	return L;
+}
+
+Lista inserirFimLista(Lista L, int n){
+	
+	return L;
+}
+
+Lista remover(Lista L, Celula * rem){
+	
+	return L;
+}
+
+Lista esvaziar(Lista L){
+	
+	return L;
+}
+
+void permutacaoJosephus(Lista L, int n, int m){  //n é o numero de pessoas no circulo e m é o passo da morte
+	
+	
+}
